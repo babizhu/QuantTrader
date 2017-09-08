497a745367a1c8ccfbc1158d740d27f741f84b55
liulaoye <liu_kun@msn.com>
2017-09-08 18:29:50 +0800

    统一的错误处理路径实验成功，看看还有没有别的优化途径，进一步统一所有的错误处理

--- src/main/java/org/bbz/stock/quanttrader/http/HttpServerVerticle.java
+++ src/main/java/org/bbz/stock/quanttrader/http/HttpServerVerticle.java
@@ -12,44 +12,43 @@ import io.vertx.ext.web.handler.BodyHandler;
 import io.vertx.ext.web.handler.CorsHandler;
 import io.vertx.ext.web.handler.StaticHandler;
 import lombok.extern.slf4j.Slf4j;
+import org.bbz.stock.quanttrader.consts.ErrorCode;
 import org.bbz.stock.quanttrader.http.handler.trade.TradeHandler;
 import org.bbz.stock.quanttrader.http.handler.user.AuthHandler;
 import org.bbz.stock.quanttrader.http.handler.user.LoginHandler;
 import org.bbz.stock.quanttrader.http.handler.user.UserHandler;
 
 /**
- * Created by liulaoye on 17-7-11.
- * web server verticle
+ * Created by liulaoye on 17-7-11. web server verticle
  */
 @Slf4j
-public class HttpServerVerticle extends AbstractVerticle{
+public class HttpServerVerticle extends AbstractVerticle {
 
-    private static final String API_PREFIX = "/api/";
-    private JWTAuth jwtAuthProvider;
-    private EventBus eventBus;
+  private static final String API_PREFIX = "/api/";
+  private JWTAuth jwtAuthProvider;
+  private EventBus eventBus;
 
-    @Override
-    public void start( Future<Void> startFuture ) throws Exception{
+  @Override
+  public void start(Future<Void> startFuture) throws Exception {
 
-        HttpServer server = vertx.createHttpServer();
-        Router router = Router.router( vertx );
-        eventBus = vertx.eventBus();
-        initHandler( router );
+    HttpServer server = vertx.createHttpServer();
+    Router router = Router.router(vertx);
+    eventBus = vertx.eventBus();
+    initHandler(router);
 
-
-        int portNumber = config().getInteger( "port", 8080 );
-        server
-                .requestHandler( router::accept )
-                .listen( portNumber, ar -> {
-                    if( ar.succeeded() ) {
-                        log.info( "HTTP server running on port " + portNumber );
-                        startFuture.complete();
-                    } else {
-                        log.error( "Could not start a HTTP server", ar.cause() );
-                        startFuture.fail( ar.cause() );
-                    }
-                } );
-    }
+    int portNumber = config().getInteger("port", 8080);
+    server
+        .requestHandler(router::accept)
+        .listen(portNumber, ar -> {
+          if (ar.succeeded()) {
+            log.info("HTTP server running on port " + portNumber);
+            startFuture.complete();
+          } else {
+            log.error("Could not start a HTTP server", ar.cause());
+            startFuture.fail(ar.cause());
+          }
+        });
+  }
 
 //    private void initAuthProvider( Future<Void> startFuture ){
 //        JsonObject config = config().getJsonObject( "mongo" );
@@ -68,52 +67,58 @@ public class HttpServerVerticle extends AbstractVerticle{
 //    }
 
 
-    private void initHandler( Router router ){
+  private void initHandler(Router router) {
 //        router.route().handler( CookieHandler.create());
 //        router.route().handler( SessionHandler.create( LocalSessionStore.create(vertx)));
 //        router.route().handler(UserSessionHandler.create(auth));  (1)
-        router.route().handler( BodyHandler.create() );
-        JsonObject jwtConfig = new JsonObject().put( "permissionsClaimKey", "roles" ).put( "keyStore", new JsonObject()
-                .put( "path", "./resources/keystore.jceks" )
-                .put( "type", "jceks" )
-                .put( "password", "secret" ) );
+    router.route().handler(BodyHandler.create());
+    JsonObject jwtConfig = new JsonObject().put("permissionsClaimKey", "roles")
+        .put("keyStore", new JsonObject()
+            .put("path", "./resources/keystore.jceks")
+            .put("type", "jceks")
+            .put("password", "secret"));
 
-        jwtAuthProvider = JWTAuth.create( vertx, jwtConfig );
-
+    jwtAuthProvider = JWTAuth.create(vertx, jwtConfig);
 
 //        router.route( API_PREFIX+"*" ).handler( new CustomJWTAuthHandlerImpl(eventBus, jwtAuthProvider ) );//暂时只能屏蔽
-        dispatcher( router );
+    dispatcher(router);
 
-        router.route().handler( StaticHandler.create() );//必须放在最后
-        router.exceptionHandler(event -> {
-            event.printStackTrace();
+    router.route().handler(StaticHandler.create());//必须放在最后
+    router.exceptionHandler(event -> {
+      event.printStackTrace();
 
-        });
-
+    });
 
 //        router.route( "/login" ).handler( this::login );
 //        router.route( "/s/isLogin" ).handler( this::isLogin );
 //        router.route( "/createUser" ).handler( this::createUser );
-    }
+  }
 
-    private void dispatcher(Router mainRouter) {
+  private void dispatcher(Router mainRouter) {
 //        mainRouter.route().handler(CorsHandler.create("vertx\\.io").allowedMethod(HttpMethod.));
 
-        final CorsHandler corsHandler = CorsHandler.create("*")
-                .maxAgeSeconds(17280000)
-                .allowedMethod(HttpMethod.GET)
-                .allowedMethod(HttpMethod.POST)
-                .allowedHeader("Content-Type");
-        mainRouter.route(API_PREFIX + "*").handler(corsHandler).failureHandler(ctx -> {
+    final CorsHandler corsHandler = CorsHandler.create("*")
+        .maxAgeSeconds(17280000)
+        .allowedMethod(HttpMethod.GET)
+        .allowedMethod(HttpMethod.POST)
+        .allowedHeader("Content-Type");
+    mainRouter.route(API_PREFIX + "*").handler(corsHandler).failureHandler(ctx -> {
 
-            ctx.response().end(ctx.failure().getMessage());
-        });
-        mainRouter.mountSubRouter(API_PREFIX, new LoginHandler(eventBus, jwtAuthProvider).addRouter(Router.router(vertx)));
+      JsonObject errorResponse = new JsonObject()
+          .put("eid", ErrorCode.SYSTEM_ERROR.toNum())
+          .put("msg", ctx.failure().getMessage());
+      ctx.response().setStatusCode(500).end(errorResponse.toString());
+    });
+    mainRouter.mountSubRouter(API_PREFIX,
+        new LoginHandler(eventBus, jwtAuthProvider).addRouter(Router.router(vertx)));
 
-        mainRouter.mountSubRouter( API_PREFIX + "trade", new TradeHandler( eventBus ).addRouter( Router.router( vertx ) ) );
-        mainRouter.mountSubRouter( API_PREFIX + "user", new UserHandler( eventBus, jwtAuthProvider ).addRouter( Router.router( vertx ) ) );
-        mainRouter.mountSubRouter( API_PREFIX + "auth", new AuthHandler( eventBus ).addRouter( Router.router( vertx ) ) );
+    mainRouter.mountSubRouter(API_PREFIX + "trade",
+        new TradeHandler(eventBus).addRouter(Router.router(vertx)));
+    mainRouter.mountSubRouter(API_PREFIX + "user",
+        new UserHandler(eventBus, jwtAuthProvider).addRouter(Router.router(vertx)));
+    mainRouter.mountSubRouter(API_PREFIX + "auth",
+        new AuthHandler(eventBus).addRouter(Router.router(vertx)));
 //        mainRouter.route(API_PREFIX+"*").handler( new ResponseHandler() );
 
-    }
+  }
 }
