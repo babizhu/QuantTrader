de12f0c4d8e2e0dec0fdcbc7cff14795d4389b9c
liulaoye <liu_kun@msn.com>
2017-11-02 18:04:24 +0800

    尝试一下kotlin，看能否解决回调问题

--- src/main/java/org/bbz/stock/quanttrader/kotlin/KotlinApp.kt
+++ src/main/java/org/bbz/stock/quanttrader/kotlin/KotlinApp.kt
@@ -2,7 +2,9 @@ package org.bbz.stock.quanttrader.kotlin
 
 import io.vertx.core.http.HttpClient
 import io.vertx.core.http.HttpClientOptions
+import io.vertx.kotlin.core.json.json
 import io.vertx.kotlin.coroutines.CoroutineVerticle
+import io.vertx.kotlin.coroutines.awaitResult
 
 //https://github.com/vert-x3/vertx-examples/blob/master/kotlin-examples/coroutines/src/main/kotlin/movierating/App.kt
 class KotlinApp : CoroutineVerticle() {
@@ -23,6 +25,18 @@ class KotlinApp : CoroutineVerticle() {
             })
         }].end()
 
+        client["/", json {  }]
 
+    }
+
+    suspend fun t1() {
+        val result = awaitResult<String> { client.queryWithParams("SELECT TITLE FROM MOVIE WHERE ID=?", json { array(id) }, it) }
+        if (result.rows.size == 1) {
+            ctx.response().end(json {
+                obj("id" to id, "title" to result.rows[0]["TITLE"]).encode()
+            })
+        } else {
+            ctx.response().setStatusCode(404).end()
+        }
     }
 }
